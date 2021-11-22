43bce1994fc632f602fca7568b73acb4f301dc7a
y0rg <y0rg@hotmail.com>
2021-11-23 00:41:10 +0100

    Add files via upload

---
+++ src/ejercicio2/DeckCards.java
@@ -0,0 +1,34 @@
+package ejercicio2;
+
+import java.util.ArrayList;
+
+public class DeckCards {
+
+	public static void main(String[] args) {
+
+		String[] suits = { "Spades", "Diamonds", "Club", "Heart" };
+		String[] values = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
+
+		ArrayList<Card> deck = new ArrayList<Card>();
+
+		for (int i = 0; i < suits.length; i++) {
+			for (int j = 0; j < values.length; j++) {
+				Card card = new Card(suits[i], values[j]);
+				deck.add(card);
+			}
+		}
+
+		for (int i = 0; i < deck.size(); i++) {
+			int j = (int) Math.floor(Math.random() * i);
+			Card tmp = deck.get(i);
+			deck.set(i, deck.get(j));
+			deck.set(j, tmp);
+		}
+
+		for (int i = 0; i < 5; i++) {
+			System.out.println(deck.get(i));
+		}
+
+	}
+
+}
