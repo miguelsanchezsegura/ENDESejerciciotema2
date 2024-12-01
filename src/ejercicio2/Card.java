fedfb25ce9e3793c167effa00226b9fd6a68bc90
MiguelSanchez <msanseg908@g.educaand.es>
2024-12-01 22:18:03 +0100

    Primer commit

--- src/ejercicio2/Card.java
+++ src/ejercicio2/Card.java
@@ -1,15 +1,15 @@
 package ejercicio2;
 
 public class Card {
-
+	//declaracion de variables
 	public String suit;
 	public String value;
-	
+	//metodo Card, constructor que define una clase carta con dos atributos, tipo o palo y valor	
 	public Card (String suit, String value) {
 		this.suit = suit;
 		this.value = value;
 	}
-	
+	//metodo toString para combinar los datos del constructor y devolver un String
 	public String toString () {
 		return (this.suit+"-"+this.value);
 	}
--- src/ejercicio2/DeckCards.java
+++ src/ejercicio2/DeckCards.java
@@ -1,34 +1,38 @@
 package ejercicio2;
-
+// libreria de java utilizada
 import java.util.ArrayList;
 
 public class DeckCards {
 
 	public static void main(String[] args) {
-
+		// crea las variables para el palo y el numero de cada carta
 		String[] suits = { "Spades", "Diamonds", "Club", "Heart" };
 		String[] values = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
 
 		ArrayList<Card> deck = new ArrayList<Card>();
-
+		// genera el mazo de cartas
 		for (int i = 0; i < suits.length; i++) {
+			//un bucle para generar 4 palos y posteriormente otro para generar 13 valores
 			for (int j = 0; j < values.length; j++) {
 				Card card = new Card(suits[i], values[j]);
 				deck.add(card);
 			}
 		}
-
+		// baraja el mazo de cartas
 		for (int i = 0; i < deck.size(); i++) {
+			//un bucle de cartas=al tamaño del mazo genera un numero aleatorio el cual se transfiere a una carta correspondiente sustituyendola en la posicion indicada
 			int j = (int) Math.floor(Math.random() * i);
 			Card tmp = deck.get(i);
 			deck.set(i, deck.get(j));
 			deck.set(j, tmp);
 		}
-
+		// genera 5 cartas
 		for (int i = 0; i < 5; i++) {
+			//imprime por pantalla 5 cartas.
 			System.out.println(deck.get(i));
 		}
 
 	}
 
 }
+
